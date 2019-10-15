1c38e25ede40104da1503eb0e58df0a6ac18bf9d
Qwizi <qwizi95@protonmail.com>
2019-10-15 18:37:21 +0200

    Added lang files

---
+++ inc/languages/english/admin/repwarning.lang.php
@@ -0,0 +1,7 @@
+<?php
+$l['id_t'] = 'User\'s ID';
+$l['id_d'] = 'Enter the user ID that will give reputation.';
+$l['comment_t'] = 'Comment';
+$l['comment_d'] = 'Comment on reputation.';
+$l['default_t'] = 'Default points';
+$l['default_d'] = 'Default reputation points';
